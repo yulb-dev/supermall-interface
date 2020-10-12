c6cda9c152c6cfadfa4b23a14d772845cfb8a1b9
yulb-dev <68833936+yulb-dev@users.noreply.github.com>
2020-10-12 19:54:43 +0800

    Update openfw.js

--- model/openfw.js
+++ model/openfw.js
@@ -1,3 +1,3 @@
 var mongoose = require('mongoose')
 // 连接数据库
-mongoose.connect("mongodb://localhost/supermall", { useNewUrlParser: true });
+mongoose.connect("mongodb://yxx:123456@47.105.222.69:27017/supermall", { useNewUrlParser: true,useUnifiedTopology: true }).then(data=>{console.log('数据库连接成功')});
